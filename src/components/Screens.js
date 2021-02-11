6b9e433bfe42c222ec96e0af5a2d6b1790894738
fanlixingit <78702670+fanlixingit@users.noreply.github.com>
2021-02-10 21:19:22 -0500

    Change title

--- src/components/Screens.js
+++ src/components/Screens.js
@@ -91,7 +91,7 @@ export default () => {
   return (
     <>
       <Header>
-        <Title>Quick Notes</Title>
+        <Title>Bryan Fan Voice Notes</Title>
         <SignOutButton
           onClick={() => {
             Auth.signOut().then(() => window.location.reload());
